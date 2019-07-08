2ab7fdcb929fd125150a029ad552819e66275464
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-08 15:32:54 -0700

    App getting started

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express"),
+	app		= express(),
+	PORT	= process.env.PORT || 3000;
+
+app.get("/", (req,res) => {
+	res.send("Hello");
+});
+
+app.listen(PORT, process.env.IP, function(){
+	console.log("Server is up!");
+});
