53df6b7b9c10c24ba05da31c93b84f1f48aa6018
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-09 19:06:26 -0700

    Change

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var express = require("express"),
-	app		= express(),
-	PORT	= process.env.PORT || 3000;
+var express 	= require("express"),
+	app			= express();
 
-app.get("/", (req,res) => {
-	res.send("Hello!");
+const PORT		= process.env.PORT || 3000,
+	bodyParser	= require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req,res){
+	res.render("index");
 });
 
 app.listen(PORT, process.env.IP, function(){
