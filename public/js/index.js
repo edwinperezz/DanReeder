58f70377e7c2be7414f61743f5e08572438bb472
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-14 17:25:46 -0700

    Lots of new changes to the home page

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ var images = [
 	"/imgs/No.7-27.jpeg",
 	"/imgs/No.41-50.jpeg",
 	"/imgs/No.19-5.jpeg",
-	"/imgs/No.19-156.jpeg",
+	"/imgs/No.7-51.jpeg",
 	"/imgs/No.15-2.jpeg"
 ]
 var time = 5000;
@@ -10,6 +10,11 @@ var i = 0;
 
 function changeImg(){
 	document.slide.src = images[i];
+	var str = images[i].split("/");
+	str = str[2].split(".",2);
+	str = str.join(".");
+	console.log(str);
+	document.getElementById("img-title").innerHTML = str;
 	if(i < images.length -1){
 		i++;
 	} else {
