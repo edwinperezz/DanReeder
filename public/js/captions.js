40c36b54e71da7a8c077e37cf0c8cda4c326f18b
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-18 16:44:31 -0700

    caption pop ups

--- public/js/captions.js
+++ public/js/captions.js
@@ -1,7 +1,24 @@
-var captions = document.getElementsByClassName("caption");
 
-for(var i = 0; i < captions.length; i++){
-	console.log("length: "+captions.length);
-	console.log(i);
-}
-console.log(captions);
+$(document).ready(function(){
+	function checkWidth(){
+		if($(window).width()>800){
+			var captions = document.getElementsByClassName("caption");
+			for(var i = 0; i < captions.length; i++){
+				captions[i].addEventListener("click",function(){
+					this.classList.toggle("caption");
+					this.classList.toggle("caption-popup");
+					$(".blank").toggleClass("h2");
+					$(".blank").toggleClass("v2");
+				});
+			}
+		} else{
+			var captions = document.getElementsByClassName("caption");
+			for(var i = 0; i < captions.length; i++){
+				captions[i].removeEventListener("click",function(){
+				});
+			}
+		}
+	}
+	checkWidth();
+	$(window).resize(checkWidth);
+});
