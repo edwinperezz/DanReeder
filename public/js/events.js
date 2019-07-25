704f2e577a51a76a941c86dcf6532134f7725eb1
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-25 04:05:34 -0700

    Changes

--- public/js/events.js
+++ public/js/events.js
@@ -7,55 +7,63 @@ $(".gallery-img:gt("+ (limitPerPage-1)+ ")").hide();
 
 
 for(var i = 1; i <= totalPages; i++){
-	$(".pagination").append("<a href='javascript:void(0)' class='current-page'>"+i+"</a>");
+	$(".page-nums").append("<a href='javascript:void(0)' class='current-page'>"+i+"</a>");
 }
 document.getElementsByClassName("current-page")[0].classList.add("active");
-$(".pagination").append("<a href='javascript:void(0)' id='next-page'>&raquo;</a>");
+// $(".pages").append("<a href='javascript:void(0)' id='next-page'>&raquo;</a>");
 $(".current-page").on("click", function(){
 	if($(this).hasClass("active")){
 		return false;
 	} else {
 		var currentPage = $(this).index();
-		$(".pagination a").removeClass("active");
+		$(".page-nums a").removeClass("active");
 		$(this).addClass("active");
 		$(".gallery-img").hide();
 		var grandTotal = currentPage * limitPerPage;
 		for(var i = (grandTotal-limitPerPage); i < grandTotal; i++){
 			$(".gallery-img:eq("+ i + ")").show();
 		}
-		window.scrollTo(0, 0);
+		var page = $(".current-page")[currentPage];
+		page.parentNode.scrollLeft = (page.offsetLeft - page.parentNode.offsetLeft);
 	}
 });
 $("#next-page").on("click", function(){
-	var currentPage = $(".pagination a.active").index();
+	var currentPage = $(".page-nums a.active").index();
+	currentPage++;
+	console.log(currentPage);
 	if(currentPage===totalPages){
 		return false;
 	} else{
 		currentPage++;
-		$(".pagination a").removeClass("active");
+		console.log(currentPage);
+		$(".page-nums a").removeClass("active");
 		$(".gallery-img").hide();
 		var grandTotal = currentPage * limitPerPage;
 		for(var i = (grandTotal-limitPerPage); i < grandTotal; i++){
 			$(".gallery-img:eq("+ i + ")").show();
 		}
 		document.getElementsByClassName("current-page")[currentPage-1].classList.add("active");
-		window.scrollTo(0, 0);
+		var page = $(".current-page")[currentPage-1];
+		page.parentNode.scrollLeft = (page.offsetLeft - page.parentNode.offsetLeft);
 
 	}
 });
 $("#previous-page").on("click", function(){
-	var currentPage = $(".pagination a.active").index();
+	var currentPage = $(".page-nums a.active").index();
+	currentPage++;
 	if(currentPage===1){
 		return false;
 	} else{
 		currentPage--;
-		$(".pagination a").removeClass("active");
+		console.log(currentPage);
+		$(".page-nums a").removeClass("active");
 		$(".gallery-img").hide();
 		var grandTotal = currentPage * limitPerPage;
 		for(var i = (grandTotal-limitPerPage); i < grandTotal; i++){
 			$(".gallery-img:eq("+ i + ")").show();
 		}
 		document.getElementsByClassName("current-page")[currentPage-1].classList.add("active");
-		window.scrollTo(0, 0);
+		var page = $(".current-page")[currentPage-1];
+		page.parentNode.scrollLeft = (page.offsetLeft - page.parentNode.offsetLeft);
 	}
 });
